38ad4893f4a7e075fec0f54135ab87271a69058c
Mohammad Ghazanfari <mgh.5225@gmail.com>
2022-11-11 14:50:45 +0330

    artefact added

--- Assets/Scripts/Artefact.cs
+++ Assets/Scripts/Artefact.cs
@@ -1,25 +1,16 @@
 using UnityEngine;
 
-public class Point : MonoBehaviour
+public class Artefact : MonoBehaviour
 {
     [SerializeField]
     private Configuration _config;
 
     [SerializeField]
-    private string _name;
-
-    [SerializeField]
-    private Light[] _lights;
-
-    [SerializeField]
-    private Artefact[] _artefacts;
-
-    public string p_name => _name;
-    public Light[] p_lights => _lights;
-
+    private GameObject _prefab;
+    private GameObject _object;
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
+        Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, _config.c_gizmos_range);
 
         var to = transform.position + transform.forward * _config.c_gizmos_length;
@@ -27,4 +18,13 @@ public class Point : MonoBehaviour
         Gizmos.DrawLine(transform.position, to);
     }
 
+    public void CreateArtefact()
+    {
+        _object = Object.Instantiate(_prefab, transform.position, transform.rotation);
+    }
+
+    public void RemoveArtefact()
+    {
+        Object.Destroy(_object);
+    }
 }
